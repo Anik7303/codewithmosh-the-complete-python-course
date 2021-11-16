d3dcd611da49b34f169de9ab4e4d64bdbd95cf0b
Anik Mohammad <anik7303@gmail.com>
2021-11-17 02:41:24 +0600

    finished section 8 - modules

---
+++ ecomerce/shopping/sales.py
@@ -0,0 +1,17 @@
+# from ecomerce.customer import contact  # absolute import (recomended)
+# from ..customer import contact  # relative import
+
+# contact.contact_customer()
+
+
+def calc_tax():
+    pass
+
+
+def calc_shipping():
+    pass
+
+
+if __name__ == "__main__":
+    print("Sales started")
+    calc_tax()
---
+++ modules.py
@@ -0,0 +1,26 @@
+# from ecomerce.shopping import sales
+
+# print(dir(sales))  # lists all attributes and methods defined in an object
+# print(sales.__name__)
+# print(sales.__package__)
+# print(sales.__file__)
+
+# from ecomerce.shopping import sales
+# from ecomerce.shopping.sales import calc_shipping
+
+# from ecomerce.sales import calc_shipping, calc_tax
+# import ecomerce.sales
+# from ecomerce import sales
+
+# from sales import calc_shipping, calc_tax
+
+# ecomerce.sales.calc_shipping() # for => import ecomerce.sales
+# sales.calc_shipping() # for => from ecomerce import sales, from ecomerce.shopping import sales
+
+# import sys
+# print(sys.path)
+
+# import sales
+
+# calc_shipping()
+# calc_tax()
